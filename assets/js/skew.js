3487a65863104f8c7ce83078e7de791b2915935f
andrea <benedetti.a93@gmail.com>
2019-03-25 11:58:40 +0100

    update

--- assets/js/skew.js
+++ assets/js/skew.js
@@ -1,16 +1,16 @@
-const section = document.querySelector("section");
-let currentPixel = window.pageYOffset;
+// const section = document.querySelector("body");
+// let currentPixel = window.pageYOffset;
 
-const looper = function () {
-	const newPixel = window.pageYOffset;
-	const diff = newPixel - currentPixel;
-	const speed = diff * 0.1;
+// const looper = function () {
+// 	const newPixel = window.pageYOffset;
+// 	const diff = newPixel - currentPixel;
+// 	const speed = diff * 0.1;
 
-	section.style.transform = "skewY(" + speed + "deg)";
+// 	section.style.transform = "skewY(" + speed + "deg)";
 
-	currentPixel = newPixel;
+// 	currentPixel = newPixel;
 
-	requestAnimationFrame(looper);
-}
+// 	requestAnimationFrame(looper);
+// }
 
-looper()
+// looper()
